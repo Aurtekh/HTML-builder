57876907a23d56d6b801c1c266178c727b308d88
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-11-08 02:23:49 +0300

    feat: complite task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir('04-copy-directory/files-copy', err => {
+  if (path.join(__dirname, '/files-copy')) {
+  } else {throw err};
+})
+
+fs.readdir(path.join(__dirname, '/files'), (err, data) => {
+  data.forEach(file => {
+    fs.copyFile(path.join(__dirname, '/files', file), path.join(__dirname, '/files-copy', file), (err) => {
+      if (err) {
+        console.log("Error Found:", err);
+      }
+  })
+  });
+});
