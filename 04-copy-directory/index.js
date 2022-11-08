493c9610be28ff0cc4fe2dc52c6fb47cc6577106
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-11-08 21:57:45 +0300

    fix: changes\delete files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir('04-copy-directory/files-copy', err => {
-  if (path.join(__dirname, '/files-copy')) {
-  } else {throw err};
-})
 
-fs.readdir(path.join(__dirname, '/files'), (err, data) => {
-  data.forEach(file => {
-    fs.copyFile(path.join(__dirname, '/files', file), path.join(__dirname, '/files-copy', file), (err) => {
-      if (err) {
-        console.log("Error Found:", err);
-      }
+fs.rm(path.join(__dirname, '/files-copy'), { recursive: true }, (err) => {
+  fs.mkdir('04-copy-directory/files-copy', { recursive: true }, (err) => {
+    if (path.join(__dirname, '/files-copy')) {
+    } else {throw err};
   })
+  fs.readdir(path.join(__dirname, '/files'), (err, data) => {
+    if (err) {
+      console.log("Error Found:", err);
+    }
+    data.forEach(file => {
+      fs.copyFile(path.join(__dirname, '/files', file), path.join(__dirname, '/files-copy', file), (err) => {
+        if (err) {
+          console.log("Error Found:", err);
+        }
+    });
   });
+});
 });
